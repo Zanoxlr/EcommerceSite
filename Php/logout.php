fcd2ea43e38cc5b44212a842e878c780abc467bd
Zanoxlr <47034106+Zanoxlr@users.noreply.github.com>
2020-05-22 21:18:30 +0200

    added comments

--- Php/logout.php
+++ Php/logout.php
@@ -1,8 +1,9 @@
 <?php
-
+// check if session exists
 if (!isset($_SESSION)) {
      header("Location: ../login.php");
 }
+// destroy the session
 session_start();
 setcookie(session_name(), '', 100,"/","localhost",true,false);
 session_unset();

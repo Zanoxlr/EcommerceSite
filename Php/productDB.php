40700dc2c79bedd2916f18f1d5b2d56bfccf5e45
Zanoxlr <47034106+Zanoxlr@users.noreply.github.com>
2020-05-22 21:19:45 +0200

    added comments

--- Php/productDB.php
+++ Php/productDB.php
@@ -1,17 +1,15 @@
 <?php
-
+// connect to the database
 include('database_connection.php');
-
+// set values
 $id = $_GET["id"];
 $queryReturn = "";
-
+// prepare the query
 $query = "SELECT * FROM produkti AS p INNER JOIN category ON category.ID = p.CategoryID INNER JOIN brand ON brand.ID = p.BrandID WHERE p.ID = $id";
-
-    
+// execute the query
 $statement = $connect->prepare($query);
 $statement->execute();
 $result = $statement->fetchAll();
-
+// return the result
 echo json_encode($result);
-
 ?>

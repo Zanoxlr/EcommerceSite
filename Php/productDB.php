b88ad0e8e34595e824ab40c63910d701859b1bda
Zanoxlr <47034106+Zanoxlr@users.noreply.github.com>
2020-05-24 21:47:18 +0200

    Update productDB.php

--- Php/productDB.php
+++ Php/productDB.php
@@ -2,7 +2,7 @@
 // connect to the database
 include('database_connection.php');
 // set values
-$id = $_GET["id"];
+$id = $_POST["id"];
 $queryReturn = "";
 // prepare the query
 $query = "SELECT * FROM produkti AS p INNER JOIN category ON category.ID = p.CategoryID INNER JOIN brand ON brand.ID = p.BrandID WHERE p.ID = $id";

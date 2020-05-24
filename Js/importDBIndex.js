ca2800b3aa390b4729273556e5d9f9d2eb2950a1
Zanoxlr <47034106+Zanoxlr@users.noreply.github.com>
2020-05-24 21:48:54 +0200

    Update importDBIndex.js

--- Js/importDBIndex.js
+++ Js/importDBIndex.js
@@ -1,8 +1,20 @@
-// when the page is loaded start BasketUpdate and
-// get items in your cart
+// when the page is loaded start BasketUpdate,
+// count items in your cart and get the filter options
 $(document).ready(function() {
     getDBresult();
     BasketUpdate();
+    filter();
+});
+// on radio button click
+$("input[type='radio']").click(getDBresult());
+// inputed text into search bar
+$('#search').keyup(function() {
+    var search = $('#search').val();
+    if (search != '') {
+        getDBresult(search);
+    } else {
+        getDBresult();
+    }
 });
 // gets the string inputed in the search bar
 // and calls getDBresult method
@@ -23,6 +35,7 @@ function getDBresult($query) {
     $PriceVal = $('input[name="Price"]:checked').val();
     $StockVal = $('input[name="Stock"]:checked').val();
     $RateVal = $('input[name="Rate"]:checked').val();
+    console.log($BrandVal);
     // POSTs the values
     $.ajax({
         type: "POST",
@@ -33,21 +46,57 @@ function getDBresult($query) {
             // parses the response to an array
             dataARR = JSON.parse(data);
             html = "";
-            // for each object in array it appends the 
-            // info into a html div
-            for (var a = 0; a < dataARR.length; a++) {
-                // gets data for each element in the object
-                var _id = dataARR[a][0];
-                var _name = dataARR[a][1];
-                var _price = dataARR[a][2];
-                var _stock = dataARR[a][4];
-                // Editing text so it fits the DIVs
-                var modStock = StockInfoMethod(_stock);
-                var modText = TextLengthEdit(_name);
-                // appending in html
-                html += "<div class='item'><picture><img onclick=\"location.href='product.php?id=" + _id + "';\" src='img/" + _id + ".webp' class='img'><p>" + modText + "</p><p>" + _price + "€</p><p>" + modStock + "</p><button onClick='AddCookies(" + _id + "," + 1 + ")'>Add</button><button onClick='AddCookies(" + _id + "," + -1 + ")'>Subtract</button><button onClick='RemoveCookies(" + _id + ")'>Remove</button></picture></div > ";
-                // adding the divs
-                document.getElementById("item").innerHTML = html;
+            // if results exist
+            if (dataARR.length != null) {
+                // for each object in array it appends the 
+                // info into a html div
+                for (var a = 0; a < dataARR.length; a++) {
+                    // gets data for each element in the object
+                    var _id = dataARR[a][0];
+                    var _name = dataARR[a][1];
+                    var _price = dataARR[a][2];
+                    var _stock = dataARR[a][4];
+                    // Editing text so it fits the DIVs
+                    var modStock = StockInfoMethod(_stock);
+                    var modText = TextLengthEdit(_name);
+                    // appending in html
+                    html += "<div class='item'><picture><img onclick=\"location.href='product.php?id=" + _id + "';\" src='img/" + _id + ".webp' class='img'><p>" + modText + "</p><p>" + _price + "€</p><p>" + modStock + "</p><button onClick='AddCookies(" + _id + "," + 1 + ")'>Add</button><button onClick='AddCookies(" + _id + "," + -1 + ")'>Subtract</button><button onClick='RemoveCookies(" + _id + ")'>Remove</button></picture></div > ";
+                    // adding the divs
+                    document.getElementById("item").innerHTML = html;
+                }
+            } else {
+                document.getElementById("item").innerHTML = "<p>no result</p>";
+            }
+        }
+    });
+}
+
+function filter() {
+    $.ajax({
+        type: "GET",
+        url: "php/filter.php",
+        success: function(data) {
+            // parses the response to an array
+            arr = JSON.parse(data);
+
+            // for each object in array in array it appends the 
+            // info into a html div as radio button
+            for (var a = 0; a < arr.length; a++) {
+                // reset html text
+                $html = "";
+                for (var b = 0; b < arr[a].length; b++) {
+                    // appends data to radio button
+                    $html = "<input type='radio' name=" + arr[a][b][0] + " value=" + arr[a][b][1] +
+                        " class='input'><label for=" + arr[a][b][0] +
+                        ">" + arr[a][b][2] +
+                        "</label><br>";
+                    // adding in the div
+                    if (a == 0) {
+                        $("#BrandID").after($html);
+                    } else if (a == 1) {
+                        $("#CategoryID").after($html);
+                    }
+                }
             }
         }
     });
